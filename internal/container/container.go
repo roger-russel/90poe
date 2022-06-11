a29c56fb613206ee8e48cc929eafd39b9fc899c1
Roger Russel <roger.russel@faci.ly>
2022-06-11 15:34:03 -0300

    refactor: add components on components struct

--- internal/container/container.go
+++ internal/container/container.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/roger.russel/90poe/internal/flags"
-	"github.com/roger.russel/90poe/pkg/component/orm"
+	"github.com/roger.russel/90poe/pkg/component/db"
 	"github.com/roger.russel/90poe/pkg/component/streamer"
 	"github.com/roger.russel/90poe/pkg/domain/port"
 )
 
 type Components struct {
+	DB       db.Inter
+	Streamer streamer.Inter
 }
 
 type Services struct {
@@ -22,11 +24,20 @@ type Dependency struct {
 }
 
 func New(ctx context.Context, flags flags.Flags) (context.Context, *Dependency, error) {
-	jsonConfig := streamer.JsonConfig{
+	jsonConfig := streamer.JSONConfig{
 		BufferParserSize: flags.ParserBufferSize,
 	}
 
-	portRepo := port.NewRepository(ctx, streamer.NewJson(jsonConfig), orm.New(ctx))
+	cmp := &Components{
+		DB:       db.New(ctx),
+		Streamer: streamer.NewJSON(jsonConfig),
+	}
+
+	repoConf := port.RepositoryConfig{
+		StreamFile: flags.File,
+	}
+
+	portRepo := port.NewRepository(ctx, repoConf, cmp.Streamer, cmp.DB)
 	port := port.New(ctx, portRepo)
 
 	dep := Dependency{
