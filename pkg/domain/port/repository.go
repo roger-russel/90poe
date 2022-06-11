26fb6514cab21d78231338d6e36acd8825f9ee1c
Roger Russel <roger.russel@faci.ly>
2022-06-11 15:38:25 -0300

    feat: add port domain service

--- pkg/domain/port/repository.go
+++ pkg/domain/port/repository.go
@@ -1,10 +1,46 @@
 package port
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/roger.russel/90poe/pkg/component/db"
+	"github.com/roger.russel/90poe/pkg/component/streamer"
+)
+
+var _ RepositoryInter = (*Repository)(nil)
+
 type RepositoryInter interface {
+	Upsert(ctx context.Context, key string, data []byte)
+	Stream(ctx context.Context, ch chan streamer.Data) error
 }
 
 type Repository struct {
+	conf RepositoryConfig
+	db   db.Inter
+	stre streamer.Inter
 }
 
 type RepositoryConfig struct {
+	StreamFile string
+}
+
+func NewRepository(ctx context.Context, conf RepositoryConfig, stre streamer.Inter, d db.Inter) *Repository {
+	return &Repository{
+		conf: conf,
+		stre: stre,
+		db:   d,
+	}
+}
+
+func (r *Repository) Stream(ctx context.Context, ch chan streamer.Data) error {
+	if err := r.stre.StreamFile(ctx, r.conf.StreamFile, ch); err != nil {
+		return fmt.Errorf("error on stream: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) Upsert(ctx context.Context, key string, data []byte) {
+	r.db.Upsert(ctx, key, data)
 }
--- pkg/domain/port/service.go
+++ pkg/domain/port/service.go
@@ -2,22 +2,48 @@ package port
 
 import (
 	"context"
+	"fmt"
+	"log"
+
+	"github.com/roger.russel/90poe/pkg/component/streamer"
 )
+
+var _ Inter = (*Service)(nil)
 
 type Inter interface {
 	Run(ctx context.Context) error
 }
 
 type Service struct {
-	repo RepositoryInter
+	repo   RepositoryInter
+	chData chan streamer.Data
 }
 
 func New(ctx context.Context, repo RepositoryInter) *Service {
 	return &Service{
-		repo: repo,
+		repo:   repo,
+		chData: make(chan streamer.Data),
 	}
 }
 
 func (s *Service) Run(ctx context.Context) error {
+	go s.upsertListener(ctx)
+
+	if err := s.repo.Stream(ctx, s.chData); err != nil {
+		return fmt.Errorf("error streaming: %w", err)
+	}
+
 	return nil
+}
+
+func (s *Service) upsertListener(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case d := <-s.chData:
+			log.Println("received key:", string(d.KeyName))
+			s.repo.Upsert(ctx, string(d.KeyName), d.KeyContent)
+		}
+	}
 }
